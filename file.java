f796792edbb6e7b14bb1fb78b49764d8a06d2415
krishna <krishna@gmail.com>
2019-09-17 10:46:29 +0530

    working area

--- file.java
+++ file.java
@@ -2,9 +2,4 @@ hello, this is my first file in git.
 some other is writing code.
 this is from master branch again
 this i want to check git pull.
-<<<<<<< HEAD
-hello this is from working area
-=======
-  
-  now this is fetch
->>>>>>> 6ae2572940992fbcf0fbe9f4c4716728ad73c9f4
+hello this is from working area.

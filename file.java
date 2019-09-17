afe5ef8a8a7658fe9491daf891a2f9194e465261
krishnagotri <39606969+krishnagotri@users.noreply.github.com>
2019-09-17 10:44:47 +0530

    Update file.java

--- file.java
+++ file.java
@@ -2,9 +2,5 @@ hello, this is my first file in git.
 some other is writing code.
 this is from master branch again
 this i want to check git pull.
-<<<<<<< HEAD
-hello this is from working area
-=======
-  
-  now this is fetch
->>>>>>> 6ae2572940992fbcf0fbe9f4c4716728ad73c9f4
+
+  this is fecth
